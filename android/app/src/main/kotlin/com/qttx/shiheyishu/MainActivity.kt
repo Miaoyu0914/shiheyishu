fb7b37de2c132a538f157ef7644fbdab78a9cf29
Ezr3a1 <yu.miao3@pactera.com>
2022-06-09 17:22:10 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/qttx/shiheyishu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qttx.shiheyishu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/configs/locales.dart
@@ -0,0 +1,12 @@
+// class Locales extends LocaleManage {
+//   Locales()
+//       : super(
+//     settings: [
+//       LocaleManageSetting(Locale('zh', 'CN'), localeZhCn),
+//       LocaleManageSetting(Locale('ja'), localeJa),
+//       LocaleManageSetting(Locale('en'), localeEn),
+//     ],
+//     storage: Get.find<StorageService>(),
+//     storageKey: StorageKeys.localeName,
+//   );
+// }
